7732f4565214c9f28f35dbd9d1ca1c2b80e17373
alexg78bis <alexandre.giannetto@gmail.com>
2018-03-14 17:55:26 +0100

    style

--- src/GSB/PlatformBundle/Repository/RapportVisiteRepository.php
+++ src/GSB/PlatformBundle/Repository/RapportVisiteRepository.php
@@ -10,26 +10,4 @@ namespace GSB\PlatformBundle\Repository;
  */
 class RapportVisiteRepository extends \Doctrine\ORM\EntityRepository
 {
-    public function getRapport($page, $nbPerPage)
-    {
-        $query = $this->createQueryBuilder('r')
-            ->leftJoin('r.medecin', 'm')
-            ->addSelect('m')
-            ->leftJoin('r.visiteur', 'v')
-            ->addSelect('v')
-            ->orderBy('r.date', 'DESC')
-            ->getQuery()
-        ;
-
-        $query
-            // On définit l'annonce à partir de laquelle commencer la liste
-            ->setFirstResult(($page-1) * $nbPerPage)
-            // Ainsi que le nombre d'annonce à afficher sur une page
-            ->setMaxResults($nbPerPage)
-        ;
-
-        // Enfin, on retourne l'objet Paginator correspondant à la requête construite
-        // (n'oubliez pas le use correspondant en début de fichier)
-        return new Paginator($query, true);
-    }
 }
