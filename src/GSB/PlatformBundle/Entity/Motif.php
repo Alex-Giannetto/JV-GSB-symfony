c34875ea2c1802cc737c895635e50308584d35ac
Alexg78bis <alexandre.giannetto@gmail.com>
2018-04-03 19:57:52 +0200

    Affichage    : 100% Suppression  : 100% Ajout        : 100% Modification : 100%

---
+++ src/GSB/PlatformBundle/Entity/Motif.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace GSB\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Motif
+ *
+ * @ORM\Table(name="motif")
+ * @ORM\Entity(repositoryClass="GSB\PlatformBundle\Repository\MotifRepository")
+ */
+class Motif
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255, unique=true)
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     *
+     * @return Motif
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+}
+
